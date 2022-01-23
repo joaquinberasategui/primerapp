768c9b7a5fe11801e0b559612982da3dc2bc5bb8
joaquinberasategui <joaquin.i.berasategui@gmail.com>
2022-01-23 20:12:33 -0300

    second Commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+
+const NavBar = () => {
+    return (
+    <nav className="nav">
+        <ul>
+            <li> Sillas </li>
+            <li> Mesas </li>
+            <li> Deco </li>
+            <li> Contacto </li>
+        </ul>
+    </nav>
+    );
+};
+
+export default NavBar;
